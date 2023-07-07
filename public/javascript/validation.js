64c34684058c88e8ce00fef32fd61099c5abc560
leovaldez51 <99807299+leovaldez51@users.noreply.github.com>
2023-07-07 17:43:08 +0530

    first commit

---
+++ public/javascript/validation.js
@@ -0,0 +1,42 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.getElementById("form1");
+
+form.addEventListener("submit", (e) => {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.value.match(mailformat)) {
+    if (password.value.length < 8) {
+      alert("Password must be atleast 8 characters");
+      e.preventDefault();
+    } else {
+      return true;
+    }
+  } else {
+    if (password.value.length < 8) {
+      alert("You have entered incorrect credentials");
+      e.preventDefault();
+    } else {
+      alert("You have entered an invalid email address!");
+      e.preventDefault();
+    }
+  }
+});
+
+// function Validation() {
+//   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   if (email.value.match(mailformat)) {
+//     if (password.value.length < 8) {
+//       alert("Password must be atleast 8 characters");
+//     } else {
+//       return true;
+//     }
+//   } else {
+//     if (password.value.length < 8) {
+//       alert("You have entered incorrect credentials");
+//       return false;
+//     } else {
+//       alert("You have entered an invalid email address!");
+//       return false;
+//     }
+//   }
+// }
